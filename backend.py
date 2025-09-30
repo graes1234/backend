377e26aa49d054147345cdb7d44af11b6540a819
graes1234 <117164702+graes1234@users.noreply.github.com>
2025-09-30 12:28:26 +0900

    Update backend.py

--- backend.py
+++ backend.py
@@ -213,6 +213,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  # Render가 주는 PORT 사용
     uvicorn.run(app, host="0.0.0.0", port=port)
     
+"""
 #중계 형식
 from fastapi import FastAPI, Request
 import os
@@ -246,6 +247,7 @@ async def predict(request: Request):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=10000)
+
 
 
 
